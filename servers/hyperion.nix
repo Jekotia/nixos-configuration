39b79b971bf822f7a404ef87cabd641e826c5cca
Josh 'Jekotia' Ameli <contact@jekotia.net>
2025-05-01 10:20:47 -0400

    fstab entries for network filesystems

--- servers/hyperion.nix
+++ servers/hyperion.nix
@@ -12,4 +12,17 @@
   # $ nix search wget
 #  environment.systemPackages = with pkgs; [
 #  ];
+
+  # fstab entries
+  fileSystems = {
+    "/multimedia" = {
+      device = "//10.0.0.10/multimedia";
+      type = "cifs";
+      options = "credentials=/home/jekotia/.smbcredentials/multimedia,uid=1000,gid=1000";
+    };
+    "/srv/ref" = {
+      device = "10.0.0.10:/mnt/storage/servers/saturn/hyperion";
+      type = "nfs";
+      options = "defaults";
+  };
 }
